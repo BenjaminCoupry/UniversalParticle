cf36190967dfa57b7b8b79e7567567409a10bd1e
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-02-18 21:29:10 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace UniversalParticle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screen = new System.Windows.Forms.PictureBox();
+            this.OfLeft = new System.Windows.Forms.Button();
+            this.OfUp = new System.Windows.Forms.Button();
+            this.OfDown = new System.Windows.Forms.Button();
+            this.OfRight = new System.Windows.Forms.Button();
+            this.ZoomIn = new System.Windows.Forms.Button();
+            this.ZoomOut = new System.Windows.Forms.Button();
+            this.LstFrm = new System.Windows.Forms.Button();
+            this.NxtFrm = new System.Windows.Forms.Button();
+            this.TimeReal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MoveScale = new System.Windows.Forms.TrackBar();
+            this.Xoff = new System.Windows.Forms.Label();
+            this.zm = new System.Windows.Forms.Label();
+            this.Yoff = new System.Windows.Forms.Label();
+            this.LogView = new System.Windows.Forms.CheckBox();
+            this.ply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveScale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Location = new System.Drawing.Point(13, 13);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(570, 450);
+            this.screen.TabIndex = 0;
+            this.screen.TabStop = false;
+            // 
+            // OfLeft
+            // 
+            this.OfLeft.Location = new System.Drawing.Point(607, 94);
+            this.OfLeft.Name = "OfLeft";
+            this.OfLeft.Size = new System.Drawing.Size(75, 23);
+            this.OfLeft.TabIndex = 1;
+            this.OfLeft.Text = "<";
+            this.OfLeft.UseVisualStyleBackColor = true;
+            this.OfLeft.Click += new System.EventHandler(this.OfLeft_Click);
+            // 
+            // OfUp
+            // 
+            this.OfUp.Location = new System.Drawing.Point(681, 65);
+            this.OfUp.Name = "OfUp";
+            this.OfUp.Size = new System.Drawing.Size(75, 23);
+            this.OfUp.TabIndex = 2;
+            this.OfUp.Text = "/\\";
+            this.OfUp.UseVisualStyleBackColor = true;
+            this.OfUp.Click += new System.EventHandler(this.OfUp_Click);
+            // 
+            // OfDown
+            // 
+            this.OfDown.Location = new System.Drawing.Point(681, 124);
+            this.OfDown.Name = "OfDown";
+            this.OfDown.Size = new System.Drawing.Size(75, 23);
+            this.OfDown.TabIndex = 3;
+            this.OfDown.Text = "\\/";
+            this.OfDown.UseVisualStyleBackColor = true;
+            this.OfDown.Click += new System.EventHandler(this.OfDown_Click);
+            // 
+            // OfRight
+            // 
+            this.OfRight.Location = new System.Drawing.Point(752, 94);
+            this.OfRight.Name = "OfRight";
+            this.OfRight.Size = new System.Drawing.Size(75, 23);
+            this.OfRight.TabIndex = 4;
+            this.OfRight.Text = ">";
+            this.OfRight.UseVisualStyleBackColor = true;
+            this.OfRight.Click += new System.EventHandler(this.OfRight_Click);
+            // 
+            // ZoomIn
+            // 
+            this.ZoomIn.Location = new System.Drawing.Point(681, 201);
+            this.ZoomIn.Name = "ZoomIn";
+            this.ZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.ZoomIn.TabIndex = 5;
+            this.ZoomIn.Text = "x1.5";
+            this.ZoomIn.UseVisualStyleBackColor = true;
+            this.ZoomIn.Click += new System.EventHandler(this.ZoomIn_Click);
+            // 
+            // ZoomOut
+            // 
+            this.ZoomOut.Location = new System.Drawing.Point(681, 231);
+            this.ZoomOut.Name = "ZoomOut";
+            this.ZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.ZoomOut.TabIndex = 6;
+            this.ZoomOut.Text = "x0.5";
+            this.ZoomOut.UseVisualStyleBackColor = true;
+            this.ZoomOut.Click += new System.EventHandler(this.ZoomOut_Click);
+            // 
+            // LstFrm
+            // 
+            this.LstFrm.Location = new System.Drawing.Point(646, 295);
+            this.LstFrm.Name = "LstFrm";
+            this.LstFrm.Size = new System.Drawing.Size(75, 23);
+            this.LstFrm.TabIndex = 7;
+            this.LstFrm.Text = "<<<";
+            this.LstFrm.UseVisualStyleBackColor = true;
+            this.LstFrm.Click += new System.EventHandler(this.LstFrm_Click);
+            // 
+            // NxtFrm
+            // 
+            this.NxtFrm.Location = new System.Drawing.Point(727, 295);
+            this.NxtFrm.Name = "NxtFrm";
+            this.NxtFrm.Size = new System.Drawing.Size(75, 23);
+            this.NxtFrm.TabIndex = 8;
+            this.NxtFrm.Text = ">>>";
+            this.NxtFrm.UseVisualStyleBackColor = true;
+            this.NxtFrm.Click += new System.EventHandler(this.NxtFrm_Click);
+            // 
+            // TimeReal
+            // 
+            this.TimeReal.AutoSize = true;
+            this.TimeReal.Location = new System.Drawing.Point(643, 379);
+            this.TimeReal.Name = "TimeReal";
+            this.TimeReal.Size = new System.Drawing.Size(45, 13);
+            this.TimeReal.TabIndex = 10;
+            this.TimeReal.Text = "Time : 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(688, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "//////////";
+            // 
+            // MoveScale
+            // 
+            this.MoveScale.LargeChange = 3;
+            this.MoveScale.Location = new System.Drawing.Point(607, 13);
+            this.MoveScale.Maximum = 100;
+            this.MoveScale.Minimum = 1;
+            this.MoveScale.Name = "MoveScale";
+            this.MoveScale.Size = new System.Drawing.Size(220, 45);
+            this.MoveScale.TabIndex = 12;
+            this.MoveScale.TickFrequency = 5;
+            this.MoveScale.Value = 1;
+            // 
+            // Xoff
+            // 
+            this.Xoff.AutoSize = true;
+            this.Xoff.Location = new System.Drawing.Point(643, 405);
+            this.Xoff.Name = "Xoff";
+            this.Xoff.Size = new System.Drawing.Size(55, 13);
+            this.Xoff.TabIndex = 13;
+            this.Xoff.Text = "Xoffset : 0";
+            // 
+            // zm
+            // 
+            this.zm.AutoSize = true;
+            this.zm.Location = new System.Drawing.Point(643, 392);
+            this.zm.Name = "zm";
+            this.zm.Size = new System.Drawing.Size(54, 13);
+            this.zm.TabIndex = 14;
+            this.zm.Text = "Zoom : 1x";
+            // 
+            // Yoff
+            // 
+            this.Yoff.AutoSize = true;
+            this.Yoff.Location = new System.Drawing.Point(643, 418);
+            this.Yoff.Name = "Yoff";
+            this.Yoff.Size = new System.Drawing.Size(55, 13);
+            this.Yoff.TabIndex = 15;
+            this.Yoff.Text = "Yoffset : 0";
+            // 
+            // LogView
+            // 
+            this.LogView.AutoSize = true;
+            this.LogView.Location = new System.Drawing.Point(681, 178);
+            this.LogView.Name = "LogView";
+            this.LogView.Size = new System.Drawing.Size(74, 17);
+            this.LogView.TabIndex = 16;
+            this.LogView.Text = "Log Scale";
+            this.LogView.UseVisualStyleBackColor = true;
+            // 
+            // ply
+            // 
+            this.ply.Location = new System.Drawing.Point(681, 324);
+            this.ply.Name = "ply";
+            this.ply.Size = new System.Drawing.Size(75, 23);
+            this.ply.TabIndex = 17;
+            this.ply.Text = "Play";
+            this.ply.UseVisualStyleBackColor = true;
+            this.ply.Click += new System.EventHandler(this.ply_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 505);
+            this.Controls.Add(this.ply);
+            this.Controls.Add(this.LogView);
+            this.Controls.Add(this.Yoff);
+            this.Controls.Add(this.zm);
+            this.Controls.Add(this.Xoff);
+            this.Controls.Add(this.MoveScale);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimeReal);
+            this.Controls.Add(this.NxtFrm);
+            this.Controls.Add(this.LstFrm);
+            this.Controls.Add(this.ZoomOut);
+            this.Controls.Add(this.ZoomIn);
+            this.Controls.Add(this.OfRight);
+            this.Controls.Add(this.OfDown);
+            this.Controls.Add(this.OfUp);
+            this.Controls.Add(this.OfLeft);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveScale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox screen;
+        private System.Windows.Forms.Button OfLeft;
+        private System.Windows.Forms.Button OfUp;
+        private System.Windows.Forms.Button OfDown;
+        private System.Windows.Forms.Button OfRight;
+        private System.Windows.Forms.Button ZoomIn;
+        private System.Windows.Forms.Button ZoomOut;
+        private System.Windows.Forms.Button LstFrm;
+        private System.Windows.Forms.Button NxtFrm;
+        private System.Windows.Forms.Label TimeReal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar MoveScale;
+        private System.Windows.Forms.Label Xoff;
+        private System.Windows.Forms.Label zm;
+        private System.Windows.Forms.Label Yoff;
+        private System.Windows.Forms.CheckBox LogView;
+        private System.Windows.Forms.Button ply;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace UniversalParticle
+{
+    public partial class Form1 : Form
+    {
+        
+        public static double zoom =1.0;
+        public static double Xoffset = 0;
+        public static double Yoffset = 0;
+        public static int Frame = 0;
+        Coord[,] Data = simulate();
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap Frm = new Bitmap(screen.Width, screen.Height);
+            screen.Image = (Image)Frm;
+        }
+        
+        
+        public static Bitmap GenerateBitmap(int xd, int yd, int frame, double zoom, double Xo, double Yo, Coord[,] datas,bool lg)
+        {
+            Bitmap Frm = new Bitmap(xd, yd);
+            for (int Part = 0; Part < datas.GetLength(1); Part++)
+            {
+                double xa = (zoom*(datas[frame,Part].X));
+                int Xaff;
+                double ya = (zoom * (datas[frame, Part].Y ));
+                int Yaff;
+                double za = zoom * datas[frame, Part].Z;
+                double Zaff;
+                if (lg)
+                {
+                    double dist = Math.Sqrt(xa * xa + ya * ya);
+                    double LogDist = Math.Log10(1.0 + dist);
+                    double Teta = Math.Sign(ya)*Math.Acos(xa/dist);
+                    Xaff = (int)(LogDist*Math.Cos(Teta) - Xo);
+                    Yaff = yd - (int)(LogDist*Math.Sin(Teta) - Yo);
+                    Zaff = 1.0 / (1.0 + Math.Exp(za/10));
+                }
+                else
+                {
+                    Xaff = (int)(xa-Xo);
+                    Yaff = yd - (int)(ya-Yo);
+                    Zaff = 1.0 / (1.0 + Math.Exp(za));
+                }
+                
+                if(((Xaff>=0)&&(Xaff<xd))&&((Yaff>=0)&&(Yaff<yd)))
+                {
+                Frm.SetPixel(Xaff, Yaff, Color.FromArgb((int)(100+154.0 * Zaff), 0, 0));
+                }
+            }
+            
+            return Frm;
+        }
+        public static Coord[,] simulate()
+        {
+            
+            int timeFrames = 100;
+            int Types = 5;
+            int partNum = 100;
+            double dt=0.005;
+            int MaxDegree = 5;
+            bool allowPositiveDegree = false;
+            bool ReciprocalInteractions = true;
+            Coord[,] Out = new Coord[timeFrames,partNum];
+            Random rand = new Random();
+            Interaction[,] interactionsList = GenerateInteractions(Types,rand,MaxDegree,allowPositiveDegree,ReciprocalInteractions);
+            Particle[] ParticleList=GenerateParticleList(partNum,Types,rand);
+            for (int T = 0; T < timeFrames; T++)
+            {
+                double t = T * dt;
+                Console.WriteLine("Time : " + t + "  Frame : " + T);
+                Coord[] ExtractedGraphData = Graphics(ParticleList);
+                for (int q = 0; q < partNum; q++)
+                {
+                    Out[T, q] = ExtractedGraphData[q];
+                }
+                for (int Concernd = 0; Concernd < ParticleList.Length; Concernd++)  
+                {
+                    Particle concerned = ParticleList[Concernd];
+                    for (int Dest = 0; Dest < ParticleList.Length; Dest++)
+                    {
+                        
+                        if(Concernd!= Dest)
+                        {
+                            Particle dest = ParticleList[Dest];
+                            double distX = dest.x-concerned.x;
+                            double distY = dest.y-concerned.y;
+                            double distZ = dest.z-concerned.z;
+                            double Dist = Math.Sqrt(Math.Pow((distX), 2) + Math.Pow(distY, 2) + Math.Pow(distZ, 2));
+                            double Intensity = interactionsList[concerned.type, dest.type].GetIntensity(dest.charge, concerned.charge, Dist);
+                            ParticleList[Concernd].Moove(dt, distX / Dist * Intensity, distY / Dist * Intensity, distZ / Dist * Intensity);
+                        }
+                    }
+                }
+                for (int Concernd = 0; Concernd < ParticleList.Length; Concernd++)
+                {
+                    ParticleList[Concernd].ApplyMoove(dt);
+                }
+                
+            }
+            
+            return Out;
+        }
+        public static Interaction[,] GenerateInteractions(int TypesNum, Random rand, int degreeRang, bool AllowPosPow, bool Simmetry)
+        {
+            Interaction[,] interactionList = new Interaction[TypesNum, TypesNum];
+            for (int i = 0; i < TypesNum; i++)
+            {
+                for (int j = 0; j < TypesNum; j++)
+                {
+                    
+                        Interaction Local = new Interaction();
+                        if (Simmetry)
+                        {
+                            if (i <= j)
+                            {
+                                Local.GenerateRandom(rand, degreeRang, AllowPosPow);
+
+                            }
+                            else
+                            {
+                                Local = interactionList[j, i];
+                            }
+                        }
+                        else
+                        {
+                            Local.GenerateRandom(rand, degreeRang, AllowPosPow);
+                        }
+                        
+                        interactionList[i, j] = Local;
+                    
+                }
+            }
+            return interactionList;
+        }
+        public static Particle[] GenerateParticleList(int N, int TypesNum, Random rand)
+        {
+            
+            Particle[] Particles = new Particle[N];
+            for (int i = 0; i < N; i++)
+            {
+                double x = rand.NextDouble() * 2.0 - 1.0;
+                double y = rand.NextDouble() * 2.0 - 1.0;
+                double z = rand.NextDouble() * 2.0 - 1.0;
+                double mass = rand.NextDouble();
+                double Charge = rand.NextDouble() * 2.0 - 1.0;
+                int type = rand.Next(0, TypesNum);
+                Particles[i] = new Particle(x,y,z,mass,Charge,type);
+            }
+            return Particles;
+        }
+        public static Coord[] Graphics(Particle[] Inputs)
+        {
+            int l= Inputs.Length;
+            Coord[] TimeData = new Coord[l];
+            for (int i = 0; i <l ; i++)
+            {
+                Coord Local = new Coord();
+                Local.X= Inputs[i].x;
+                Local.Y = Inputs[i].y;
+                Local.Z = Inputs[i].z;
+                TimeData[i] = Local;
+            }
+            return TimeData;
+        }
+
+        
+        
+        private void OfUp_Click(object sender, EventArgs e)
+        {
+            Yoffset += MoveScale.Value;
+            Yoff.Text = "Yoffset : " + Yoffset;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data,LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void OfLeft_Click(object sender, EventArgs e)
+        {
+            Xoffset -= MoveScale.Value;
+            Xoff.Text = "Xoffset : " + Xoffset;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void OfDown_Click(object sender, EventArgs e)
+        {
+            Yoffset -=  MoveScale.Value;
+            Yoff.Text = "Yoffset : " + Yoffset;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void OfRight_Click(object sender, EventArgs e)
+        {
+            Xoffset += MoveScale.Value;
+            Xoff.Text = "Xoffset : " + Xoffset;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void ZoomIn_Click(object sender, EventArgs e)
+        {
+            zoom *= 1.5;
+            if (zoom >= 1)
+            {
+                zm.Text = "Zoom : " + (int)zoom + "x";
+            }
+            else
+            {
+                zm.Text = "Zoom : " + ((float)((int)(10000.0*zoom)))/10000.0 + "x";
+            }
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void ZoomOut_Click(object sender, EventArgs e)
+        {
+            zoom *= 0.5;
+            if (zoom >= 1)
+            {
+                zm.Text = "Zoom : " + (int)zoom + "x";
+            }
+            else
+            {
+                zm.Text = "Zoom : " + ((float)((int)(10000.0 * zoom))) / 10000.0 + "x";
+            }
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void LstFrm_Click(object sender, EventArgs e)
+        {
+            Frame -= 1;
+            if (Frame < 0)
+            {
+                Frame = 0;
+            }
+            TimeReal.Text = "Time : " + Frame;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void NxtFrm_Click(object sender, EventArgs e)
+        {
+            Frame += 1;
+            Frame = Frame % Data.GetLength(0);
+            TimeReal.Text = "Time : " + Frame;
+            Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, Frame, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+            screen.Image = (Image)Frm;
+        }
+
+        private void ply_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < Data.GetLength(0); i++)
+            {
+                
+                Bitmap Frm = GenerateBitmap(screen.Width, screen.Height, i, zoom, Xoffset, Yoffset, Data, LogView.Checked);
+                System.Threading.Thread.Sleep(10);
+                TimeReal.Text = "Time : " + i;
+                TimeReal.Refresh();
+                screen.Image = (Image)Frm;
+                screen.Refresh();
+                
+            }
+        }
+    }
+    
+    
+    public class Particle
+    {
+        public double x;
+        public double y;
+        public double z;
+        double dx = 0;
+        double dy = 0;
+        double dz = 0;
+        public double mass;
+        public double charge;
+        public int type;
+        public Particle(double x_, double y_, double z_, double mass_, double charge_, int type_)
+        {
+            x = x_;
+            y = y_;
+            z = z_;
+            mass = mass_;
+            charge = charge_;
+            type = type_;
+        }
+        public void Moove (double dt,double ddx, double ddy,double ddz)
+        {
+            if ((!double.IsNaN(ddx)) && (!double.IsNaN(ddy)) && (!double.IsNaN(ddz)))
+            {
+                dx += dt * ddx / mass;
+                dy += dt * ddy / mass;
+                dz += dt * ddz / mass;
+            }
+            
+
+        }
+        public void ApplyMoove(double dt)
+        {
+            if ((!double.IsNaN(dx)) && (!double.IsNaN(dy)) && (!double.IsNaN(dz)))
+            {
+                x += dx * dt;
+                y += dy * dt;
+                z += dz * dt;
+            }
+        }
+    }
+    public class Interaction
+    {
+        List<double> Coeffs = new List<double>();
+        List<int> Degrees = new List<int>();
+        public double GetIntensity(double charge1, double charge2, double dist)
+        {
+            double intensity = 0;
+            if (dist != 0)
+            {
+               
+                for (int i = 0; i < Coeffs.Count; i++)
+                {
+                    intensity += Coeffs.ElementAt(i) * Math.Pow(dist, Degrees.ElementAt(i));
+                }
+                intensity = intensity * charge1 * charge2;
+                
+            }
+            return intensity;
+        }
+        public void GenerateRandom(Random rand, int degreeRange, bool AllowPositiveDegree)
+        {
+            int DegreeNum = rand.Next(1, 10);
+            int MaxDegree;
+            if (AllowPositiveDegree)
+            { 
+                MaxDegree=rand.Next(1, degreeRange);
+            }
+            else
+            {
+                MaxDegree = 0;
+            }
+            int MinDegree = -1*rand.Next(1, degreeRange);
+            for (int i = 0; i < DegreeNum; i++)
+            {
+                Coeffs.Add(rand.NextDouble() * 2.0 - 1);
+                Degrees.Add(rand.Next(MinDegree,MaxDegree));
+            }
+        }
+    }
+    public class Coord
+    {
+        public double X;
+        public double Y;
+        public double Z;
+    }
+    
+}
